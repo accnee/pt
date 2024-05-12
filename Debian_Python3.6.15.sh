fb351472a76f3442cd8755c17693c8dea00b2917
accnee <105717734+accnee@users.noreply.github.com>
2024-05-12 19:19:13 +0800

    Update Debian_Python3.6.15.sh

--- Debian_Python3.6.15.sh
+++ Debian_Python3.6.15.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 apt-get update -y
 apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz && tar -zxvf Python-3.6.15.tgz && cd Python-3.6.15
