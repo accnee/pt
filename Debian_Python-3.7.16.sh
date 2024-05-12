cbe82d48cb865190152732d0c87cd830c93365b1
accnee <105717734+accnee@users.noreply.github.com>
2024-05-12 11:33:27 +0800

    Update Debian_Python-3.7.16.sh

--- Debian_Python-3.7.16.sh
+++ Debian_Python-3.7.16.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 apt-get update -y
 apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-wget https://www.python.org/ftp/python/3.7.16/Python-3.7.16.tar.xz && tar xvf Python-3.7.16.tar.xz && cd Python-3.7.16
+wget https://www.python.org/ftp/python/3.7.16/Python-3.7.16.tgz && tar xzvf Python-3.7.16.tgz && cd Python-3.7.16
 ./configure
 make&& make install
 cd ..
-rm -rf Python-3.7.16 Python-3.7.16.tar.xz
+rm -rf Python-3.7.16 Python-3.7.16.tgz
